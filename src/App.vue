46881bd1546d24b610ede5ce6dc111ab12295d23
zhangjiangtao <zhjt_88@163.comzhjt_88@163.comzhjt_88@163.com>
2024-09-22 15:28:55 +0800

    tet

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,32 @@
-<template>
-  <div>Hello {{ value }}</div>
-</template>
 
+<template>
+  <div>
+    <h1>User Data</h1>
+    <p>{{ userData }}</p>
+  </div>
+</template>
 <script>
-export default {
-  name: "App",
-  data() {
-    return {
-      value: "World"
-    };
-  }
-};
+import axios from 'axios';
+ 
+ export default {
+   data() {
+     return {
+       userData: null
+     };
+   },
+   created() {
+     this.fetchUserData();
+   },
+   methods: {
+     fetchUserData() {
+       axios.get('https://app-240920125718.azurewebsites.net/api/greeting')
+         .then(response => {
+           this.userData = response.data;
+         })
+         .catch(error => {
+           console.error('There was an error!', error);
+         });
+     }
+   }
+ };
 </script>
